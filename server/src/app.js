d93492c68b82d9862ac1b089e7e0ff9934506e9f
Yulian Amstislavski <yuliana@nayax.com>
2021-03-24 10:12:52 +0200

    many changes server and client

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,9 @@
 console.log('nice');
+const express = require('express')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+const morgan = require('moragn')
+
+const app = express()
+
+app.use(moragn('combine'))
