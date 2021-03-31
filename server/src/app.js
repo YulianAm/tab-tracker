3693193789659a656464bd34a11841f9d75121c8
Yulian Amstislavski <yuliana@nayax.com>
2021-03-31 15:21:12 +0300

    standart commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,19 @@
-console.log('nice');
 const express = require('express')
 const bodyparser = require('body-parser')
 const cors = require('cors')
-const morgan = require('moragn')
+const morgan = require('morgan')
 
 const app = express()
 
-app.use(moragn('combine'))
+app.use(morgan('combined'))
+app.use(bodyparser.json());
+app.use(cors())
+
+app.get('/register', (req, res) => {
+ res.send({
+     message: `${req.body.email} your user was registered with password ${req.body.password}`
+ })
+})
+
+app.listen(process.env.PORT || 8082)
+
